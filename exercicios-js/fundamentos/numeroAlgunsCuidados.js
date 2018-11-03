9432ae6e75d41f7dc8702a955a4aa60fd958f619
MathiTz <matheusalves789@gmail.com>
2018-11-03 13:14:43 -0300

    Alguns cuidados para com números

---
+++ exercicios-js/fundamentos/numeroAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) // retorna infinito
+console.log("10" / 2) 
+console.log("Show!" * 2) // não retorna pois é string
+console.log(0.1 + 0.7) // retorna valor não inteiro
+//console.log(10.toString())
+console.log((10.345).toFixed(2))
