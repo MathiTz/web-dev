40ce62b42d94b00276505103ef52cc17af77f2bc
MathiTz <matheusalves789@gmail.com>
2018-11-03 14:32:51 -0300

    Mais um uso de concatenação

--- exercicios-js/fundamentos/numeroAlgunsCuidados.js
+++ exercicios-js/fundamentos/numeroAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7 / 0) // retorna infinito
-console.log("10" / 2) 
+console.log("10" / 2)
+console.log('3' + 2) // concatena 
 console.log("Show!" * 2) // não retorna pois é string
 console.log(0.1 + 0.7) // retorna valor não inteiro
 //console.log(10.toString())
--- exercicios-js/fundamentos/strings.js
+++ exercicios-js/fundamentos/strings.js
@@ -7,5 +7,6 @@ console.log(escola.indexOf('3')) // retorna o indice do valor
 console.log(escola.substring(1)) // a partir de 
 console.log(escola.substring(0,3)) // vai de até n-1
 console.log('Escola '.concat(escola).concat("!")) // concatenar
+console.log('Escola ' + escola + "!") // concatenar
 console.log(escola.replace(3, 'e')) // troca o índice
 console.log('Ana,Maria,Pedro'.split(',')) // transforma em array
