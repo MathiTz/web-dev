405f334cc81a50e9a47d23b9c4664f4a4ef70ec5
MathiTz <matheusalves789@gmail.com>
2018-11-03 12:34:44 -0300

    Var, let, const

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,16 @@
+var a = 3
+let b = 4 // só pode ser declarado uma vez
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5 // só pode ser atribuido uma vez
+// c = 50
+console.log(c)
