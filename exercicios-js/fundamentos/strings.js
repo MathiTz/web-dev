ed6fbc468aa014284ce734ecb020b1537b4b5c89
MathiTz <matheusalves789@gmail.com>
2018-11-03 14:30:07 -0300

    Uso de string com funções

---
+++ exercicios-js/fundamentos/strings.js
@@ -0,0 +1,11 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4)) // letra no índice 4
+console.log(escola.charAt(5)) // ele dá vazio
+console.log(escola.charCodeAt(3)) // tabela asc
+console.log(escola.indexOf('3')) // retorna o indice do valor
+console.log(escola.substring(1)) // a partir de 
+console.log(escola.substring(0,3)) // vai de até n-1
+console.log('Escola '.concat(escola).concat("!")) // concatenar
+console.log(escola.replace(3, 'e')) // troca o índice
+console.log('Ana,Maria,Pedro'.split(',')) // transforma em array
