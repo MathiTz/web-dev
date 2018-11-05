6f3bc314e14caad04a79e6863141c16b1b69e975
MathiTz <matheusalves789@gmail.com>
2018-11-04 23:15:32 -0300

    Uso de Null e Undefined

---
+++ exercicios-js/fundamentos/nullUndefined.js
@@ -0,0 +1,24 @@
+let valor // não inicializada
+console.log(valor)
+
+valor = null //ausencia de valor
+console.log(valor)
+//console.log(valor.toString()) // Erro!
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco =undefined // evite atribuir undefined
+// deixe a linguagem definir 
+
+console.log(!!produto.preco)
+// delete produto.preco
+console.log(produto)
+
+produto.preco = null // sem preço
+console.log(!!produto.preco)
+console.log(produto)
--- exercicios-js/fundamentos/objeto.js
+++ exercicios-js/fundamentos/objeto.js
@@ -10,4 +10,4 @@ const prod2 = {
     preco: 79.90
 }
 
-console.log(prod2)
+console.log(prod2)      
