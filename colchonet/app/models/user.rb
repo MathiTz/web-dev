4fb743c90e8aac2a509ed959bbd151b8d783ab57
MathiTz <matheusalves789@gmail.com>
2018-12-02 23:59:16 -0300

    Creation of info validation

--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
+    EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+    
     validates_presence_of :email, :full_name, :location, :password
     validates_confirmation_of :password
     validates_length_of :bio, minimum: 30, allow_blank: false
+    validates_uniqueness_of :email
+
+        validate do
+            errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+        end    
+    
+    =begin
+        private
+        def email_format
+            errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+            end
+    =end
 end
