e6930ecea4644aed0dbcd7f213d4ccaef6fb3cb0
MathiTz <matheusalves789@gmail.com>
2018-12-04 00:48:01 -0300

    Criado validações para criação do quarto

--- colchonet/app/models/room.rb
+++ colchonet/app/models/room.rb
@@ -2,4 +2,7 @@ class Room < ApplicationRecord
     def complete_name
         "#{title}, #{location}"
     end
+
+    validates_presence_of :title, :location
+    validates_length_of :description, minimum: 30, allow_blank: false
 end
