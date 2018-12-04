05974b4adc33a273f03d0fc36ff60238b5249bba
MathiTz <matheusalves789@gmail.com>
2018-12-03 23:21:17 -0300

    Criado migração de senhas encriptadas

--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord
     EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
     
-    validates_presence_of :email, :full_name, :location, :password
-    validates_confirmation_of :password
+    validates_presence_of :email, :full_name, :location
     validates_length_of :bio, minimum: 30, allow_blank: false
-    validates_uniqueness_of :email
-
-        validate do
-            errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
-        end    
+    validates_format_of :email, with: EMAIL_REGEXP
+    
+    has_secure_password
     
     #=begin
         #private
