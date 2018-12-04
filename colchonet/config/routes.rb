391780a557a85eed126bee8f695d2b4a2908db89
MathiTz <matheusalves789@gmail.com>
2018-12-04 10:51:02 -0300

    Acrescento da funcionalidade de tradução para páginas

--- colchonet/config/routes.rb
+++ colchonet/config/routes.rb
@@ -1,7 +1,11 @@
 Colchonet::Application.routes.draw do
+  LOCALES = /en|pt\-BR/
+
+  scope "(:locale)", locale: /en|pt|-BR/ do
   resources :rooms
   resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/:locale' => 'home#index', locale: LOCALES
   root 'home#index'
 end
