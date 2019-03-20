45801629c49bf0e10539d179ecdf726b14a6c22a
mathitz <matheusalves789@gmail.com>
2019-03-19 21:28:08 -0300

    CRUD with JS

---
+++ avancandoComJs/curso/js/config.js
@@ -0,0 +1,9 @@
+function setConfig() {
+    const texts = {
+        "title": "Shopping Control"
+    };
+    document.title = texts.title;
+    document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+setConfig();
---
+++ avancandoComJs/curso/js/main.js
@@ -0,0 +1,109 @@
+let list = [
+    {
+        "desc":"rice",
+        "amount":"1",
+        "value":"5.40"
+    },
+    {
+        "desc":"beer",
+        "amount":"12",
+        "value":"1.99"
+    },
+    {
+        "desc":"meat",
+        "amount":"1",
+        "value":"15.00"
+    }
+];
+
+function getTotal(list) {
+    let total = 0;
+    for(let key in list){
+        total += list[key].value * list[key].amount;
+    }
+    return total
+}
+
+function setList(list) {
+    let table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
+    for(let key in list){
+        table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-primary" onclick="setUpdate('+key+');">Edit</button> | <button class="btn btn-danger" onclick="deleteData('+key+');">Delete </button></td></tr>'
+    }
+    table += '</tbody>';
+    document.getElementById("listTable").innerHTML = table;
+}
+
+function formatDesc(desc) {
+    let str = desc.toString().toLowerCase();
+    str = str.charAt(0).toUpperCase() + str.slice(1);
+    return str;
+}
+
+function formatValue(value) {
+    let str = parseFloat(value).toFixed(2) + "";
+    str = str.replace(".", ",");
+    str = "$ " + str;
+    return str;
+}
+
+function addData() {
+    let desc = document.getElementById("desc").value;
+    let amount = document.getElementById("amount").value;
+    let value = document.getElementById("value").value;
+
+    list.unshift({"desc":desc, "amount":amount, "value":value});
+    setList(list);
+}
+
+function setUpdate(id) {
+    let obj = list[id];
+    document.getElementById("desc").value = obj.desc;
+    document.getElementById("amount").value = obj.amount;
+    document.getElementById("value").value = obj.value;
+    document.getElementById("btnUpdate").style.display = "inline-block";
+    document.getElementById("btnAdd").style.display = "none";
+
+    document.getElementById("inputIDUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="'+id+'">';
+}
+
+function resetForm() {
+    document.getElementById("desc").value = "";
+    document.getElementById("amount").value = "";
+    document.getElementById("value").value = "";
+    document.getElementById("btnUpdate").style.display = "none";
+    document.getElementById("btnAdd").style.display = "inline-block";
+    document.getElementById("inputIDUpdate").innerHTML = "";
+}
+
+function updateData(){
+    let id = document.getElementById("idUpdate").value;
+    let desc = document.getElementById("desc").value;
+    let amount = document.getElementById("amount").value;
+    let value = document.getElementById("value").value;
+
+    list[id] = {"desc":desc, "amount":amount, "value": value};
+    resetForm();
+    setList(list);
+
+}
+
+function deleteData(id){
+    if(confirm("Delete this item?"))
+    {
+        if(id === list.length - 1){
+            list.pop();
+        } else if(id === 0)
+        {
+            list.shift();
+        } else {
+            let arrAuxIni = list.slice(0,id);
+            let arrAuxEnd = list.slice(id + 1);
+            list = arrAuxIni.concat(arrAuxEnd);
+        }
+        setList(list);
+    }
+}
+
+setList(list);
+
+// console.log(getTotal(list));
