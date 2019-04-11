b0ea70fcad121c19d435b21dddfff2681aac6f24
Mathitz <matheusalves789@gmail.com>
2019-04-10 21:12:09 -0300

    Start with ruby's study

---
+++ rubyStudy/operators.rb
@@ -0,0 +1,24 @@
+number1 = 0 #int
+number2 = '0' #char
+
+# puts number1.equal? number2 // estritamente igual
+# puts number1.eql? number2 // checa se o object id é igual
+
+=begin 
+puts 'Number 1 :'
+number1 = STDIN.gets
+number1 = number1.to_i
+
+puts 'Number 2 :'
+number2 = STDIN.gets
+number2 = number2.to_i
+=end
+
+# total = number1 + number2
+# total = number1 - number2
+# total = number1 * number2
+# total = number1 / number2
+# total = number1 ** number2
+# total = number1 % number2
+
+puts 'Result : ' + total.to_s 
