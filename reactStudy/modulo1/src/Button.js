f5e16058fceee7283ebb08ea2164cd801b2cb2dc
Mathitz <matheusalves789@gmail.com>
2019-04-16 22:05:08 -0300

    Favoritador de repositórios

--- reactStudy/modulo1/src/Button.js
+++ reactStudy/modulo1/src/Button.js
@@ -1,21 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
+// import React from 'react';
+// import PropTypes from 'prop-types';
 
-const Button = (props) => {
-  return <button onClick = {
-    props.onClick
-  } > {
-    props.children
-  } < /button>
-};
+// const Button = props => (
+//   <button onClick={props.onClick}>
+//     {' '}
+//     {props.children}
+//     {' '}
+//   </button>
+// );
 
-Button.defaultProps = {
-  children: 'Salvar',
-};
+// Button.defaultProps = {
+//   children: 'Salvar',
+// };
 
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.string,
-};
+// Button.propTypes = {
+//   onClick: PropTypes.func.isRequired,
+//   children: PropTypes.string,
+// };
 
-export default Button;
+// export default Button;
