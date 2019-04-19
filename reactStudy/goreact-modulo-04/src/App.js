fb54e32e4df0d128b202fa935d3e2489571df5e1
Mathitz <matheusalves789@gmail.com>
2019-04-19 00:10:51 -0300

    Estilizando header

---
+++ reactStudy/goreact-modulo-04/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from 'react';
+
+import GlobalStyle from './styles/global';
+
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+
+import { Wrapper, Container } from './styles/components';
+
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Wrapper>
+      <Container>
+        <Sidebar />
+      </Container>
+      <Player />
+    </Wrapper>
+  </Fragment>
+);
+
+export default App;
---
+++ reactStudy/goreact-modulo-04/src/components/Player/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import Slider from 'rc-slider';
+
+import {
+  Container, Current, Volume, Controls, Progress, Time, ProgressSlider,
+} from './style';
+
+import VolumeIcon from '../../assets/images/volume.svg';
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
+
+const Player = () => (
+  <Container>
+    <Current>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/pt/thumb/1/18/220px-SystemofaDownToxicityalbumcover.jpg/220px-220px-SystemofaDownToxicityalbumcover.jpg"
+        alt="Cover"
+      />
+
+      <div>
+        <span>Toxicity</span>
+        <small>System of a down</small>
+      </div>
+    </Current>
+
+    <Progress>
+      <Controls>
+        <button>
+          <img src={ShuffleIcon} alt="Shuffle" />
+        </button>
+        <button>
+          <img src={BackwardIcon} alt="Backward" />
+        </button>
+        <button>
+          <img src={PlayIcon} alt="Play" />
+        </button>
+        <button>
+          <img src={PauseIcon} alt="Pause" />
+        </button>
+        <button>
+          <img src={ForwardIcon} alt="Forward" />
+        </button>
+        <button>
+          <img src={RepeatIcon} alt="Repeat" />
+        </button>
+      </Controls>
+      <Time>
+        <span>1:39</span>
+        <ProgressSlider>
+          <Slider
+            railStyle={{ background: '#404040', borderRadius: 10 }}
+            trackStyle={{ background: '#1ED760' }}
+            handleStyle={{ border: 0 }}
+          />
+        </ProgressSlider>
+        <span>4:39</span>
+      </Time>
+    </Progress>
+
+    <Volume>
+      <img src={VolumeIcon} alt="Volume" />
+      <Slider
+        railStyle={{ background: '#404040', boderRadius: 10 }}
+        trackStyle={{ background: '#FFF' }}
+        handleStyle={{ display: 'none' }}
+        value={100}
+      />
+    </Volume>
+  </Container>
+);
+
+export default Player;
