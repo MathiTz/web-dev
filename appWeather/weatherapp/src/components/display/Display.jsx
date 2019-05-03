5da1119fc3bef4f326f1e081efe403142008e5a9
Matheus Alves <matheusalves@Amandios-MacBook-Pro.local>
2019-05-03 13:24:23 -0300

    Shape of info for search input

--- appWeather/weatherapp/src/components/display/Display.jsx
+++ appWeather/weatherapp/src/components/display/Display.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { Fragment, Component } from "react";
+import "./Display.css";
 
-const Display = () => <h1>Previsão do Tempo</h1>;
-
-export default Display;
+export default class Display extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Previsão do tempo</h1>
+        <div className="white-square">
+          <aside>
+            <small>Cidade, ES - País</small>
+          </aside>
+          <h1>Capital</h1>
+        </div>
+      </Fragment>
+    );
+  }
+}
