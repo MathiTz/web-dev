e0ddd89c5880cefc5dc667106671ac7c60c7f170
Mathitz <matheusalves789@gmail.com>
2019-05-02 22:47:02 -0300

    Componente de Classe 1

---
+++ cod3r-web-dev/exercicios-web/react/exercicios/src/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment, Component } from "react";
+import ReactDOM from "react-dom";
+
+import Multi, { BoaNoite } from "./componentes/Multiplos";
+
+ReactDOM.render(
+  <div>
+    <Multi.BoaTarde nome="Ana" />
+    <BoaNoite nome="Bia" />
+  </div>,
+  document.getElementById("root")
+);
