7317ca2faf773c109ebba0be52d9c9f575a3a844
Mathitz <matheusalves789@gmail.com>
2019-05-14 21:16:11 -0300

    Continuando curso cod3r

--- cod3r-web-dev/exercicios-web/banco-relacional/consultarEstados.sql
+++ cod3r-web-dev/exercicios-web/banco-relacional/consultarEstados.sql
@@ -1,2 +1,16 @@
 select *
 from estados
+
+select
+  sigla,
+  nome as 'Nome do Estado'
+from estados
+where regiao = 'Sul'
+
+-- select sigla, nome 'Nome do Estado' from estados
+-- where regiao = 'Sul'
+
+select nome, regiao
+from estados
+where populacao >= 10
+order by populacao desc
