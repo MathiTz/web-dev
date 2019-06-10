54705e0ac8d17ef92c87f969cdb13cafbdc93b9e
Matheus Alves <matheusalves@Mac-mini-de-Admin.local>
2019-06-10 13:13:53 -0300

    Buttons event

--- studyJS/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
+++ studyJS/curso-javascript-projeto-calculadora-clone/scripts/controller/CalcController.js
@@ -1,22 +1,63 @@
 class CalcController {
   constructor() {
-    this._displayCalc = "0";
-    this._dataAtual;
+    this._locale = "pt-BR";
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    this.setDisplayDateTime();
+
+    setInterval(() => {
+      this.setDisplayDateTime();
+    }, 1000);
+  }
+
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit",
+      month: "long",
+      year: "numeric"
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    return (this._timeEl.innerHTML = value);
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    return (this._dateEl.innerHTML = value);
   }
 
   get displayCalc() {
-    return this._displayCalc;
+    return this._displayCalcEl.innerHTML;
   }
 
   set displayCalc(value) {
     this._displayCalc = value;
   }
 
-  get dataAtual() {
-    return this._dataAtual;
+  get currentDate() {
+    return new Date();
   }
 
-  set dataAtual(value) {
-    this._dataAtual = value;
+  set currentDate(value) {
+    this._currentDate = value;
   }
 }
