d82b416a03d1452d9178a39ea4ac9e253c9f590f
Mathitz <matheusalves789@gmail.com>
2019-06-20 20:44:50 -0300

    Componente Funcional

---
+++ cod3r-web-dev/exercicios-react/src/componentes/FamiliaSilva.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Membro from "./Membro";
+
+export default props => (
+  <div>
+    <Membro name="Rafael" sobrenome="Silva" />
+    <Membro name="Daniela" sobrenome="Silva" />
+    <Membro name="Pedro" sobrenome="Silva" />
+    <Membro name="Ana" sobrenome="Silva" />
+  </div>
+);
--- cod3r-web-dev/exercicios-react/src/index.js
+++ cod3r-web-dev/exercicios-react/src/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import PrimeiroComponente from "./componentes/PrimeiroComponente";
+import CompA, { CompB as B } from "./componentes/DoisComponentes";
+import MultiElementos from "./componentes/MultiElementos";
+import FamiliaSilva from "./componentes/FamiliaSilva";
+import Familia from "./componentes/Familia";
+import Membro from "./componentes/Membro";
 
 const elemento = document.getElementById("root");
-const jsx = <h1>Teste</h1>;
-ReactDOM.render(jsx, elemento);
+ReactDOM.render(
+  <div>
+    <Familia sobrenome="Silva">
+      <Membro name="Andre" />
+      <Membro name="Mariana" />
+    </Familia>
+    {/* <FamiliaSilva /> */}
+    {/* <MultiElementos /> */}
+    {/*<CompA valor="Olá eu sou A!" />
+    <B valor="B na área" /> */}
+    {/* <PrimeiroComponente valor="Bom dia!" /> */}
+  </div>,
+  elemento
+);
+
+// const jsx = <h1>Teste</h1>;
+// ReactDOM.render(jsx, elemento);
