35559482c6361bfe4f93702cd77ccaf3090663ac
Mathitz <matheusalves789@gmail.com>
2019-06-20 22:50:27 -0300

    Feito fundamentos de React

--- cod3r-web-dev/exercicios-react/src/index.js
+++ cod3r-web-dev/exercicios-react/src/index.js
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import PrimeiroComponente from "./componentes/PrimeiroComponente";
-import CompA, { CompB as B } from "./componentes/DoisComponentes";
-import MultiElementos from "./componentes/MultiElementos";
-import FamiliaSilva from "./componentes/FamiliaSilva";
-import Familia from "./componentes/Familia";
-import Membro from "./componentes/Membro";
+// import CompA, { CompB as B } from "./componentes/DoisComponentes";
+// import MultiElementos from "./componentes/MultiElementos";
+// import FamiliaSilva from "./componentes/FamiliaSilva";
+// import Familia from "./componentes/Familia";
+// import Membro from "./componentes/Membro";
+// import ComponenteComFuncao from "./componentes/ComponenteComFuncao";
+// import Pai from "./componentes/Pai";
+// import ComponenteClasse from "./componentes/ComponenteClasse";
+import Contador from "./componentes/Contador";
 
 const elemento = document.getElementById("root");
 ReactDOM.render(
   <div>
-    <Familia sobrenome="Silva">
+    <Contador numeroInicial={100} />
+    {/* <ComponenteClasse /> */}
+    {/* <Pai /> */}
+    {/* <ComponenteComFuncao /> */}
+    {/* <Familia sobrenome="Silva">
       <Membro name="Andre" />
       <Membro name="Mariana" />
-    </Familia>
+    </Familia> */}
     {/* <FamiliaSilva /> */}
     {/* <MultiElementos /> */}
     {/*<CompA valor="Olá eu sou A!" />
