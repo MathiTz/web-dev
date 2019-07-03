11d8f05b6e713d36d3ab1b35eb9254886f08dcd5
Mathitz <matheusalves789@gmail.com>
2019-07-03 12:09:22 -0300

    MVC

--- studyJS/curso-javascript-projeto-usuarios/index.js
+++ studyJS/curso-javascript-projeto-usuarios/index.js
@@ -2,6 +2,22 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 
 var user = {};
 
+function addLine(dataUser) {
+  console.log(dataUser);
+
+  document.getElementById("table-users").innerHTML = `
+    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+    <td>${dataUser.name}</td>
+    <td>${dataUser.email}</td>
+    <td>${dataUser.admin}</td>
+    <td>${dataUser.birth}</td>
+    <td>
+      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+    </td>
+  `;
+}
+
 document
   .getElementById("form-user-create")
   .addEventListener("submit", function(event) {
@@ -17,5 +33,16 @@ document
       }
     });
 
-    console.log(user);
+    var objectUser = new User(
+      user.name,
+      user.gender,
+      user.birth,
+      user.country,
+      user.email,
+      user.password,
+      user.photo,
+      user.admin
+    );
+
+    addLine(objectUser);
   });
