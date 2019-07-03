cc868ca931cdd222e5e0d6bab5f6690ab0bea9ab
Mathitz <matheusalves789@gmail.com>
2019-07-02 23:19:54 -0300

    Trabalhando com funções

--- studyJS/curso-javascript-projeto-usuarios/index.js
+++ studyJS/curso-javascript-projeto-usuarios/index.js
@@ -1,19 +1,21 @@
-var nome = document.querySelector("#exampleInputName");
+var fields = document.querySelectorAll("#form-user-create [name]");
 
-var gender = document.querySelectorAll(
-  "#form-user-create [name=gender]:checked"
-);
+var user = {};
 
-console.log(gender);
+document
+  .getElementById("form-user-create")
+  .addEventListener("submit", function(event) {
+    event.preventDefault();
 
-var birth = document.querySelector("#exampleInputBirth");
+    fields.forEach(function(field, index) {
+      if (field.name == "gender") {
+        if (field.checked) {
+          user[field.name] = field.value;
+        }
+      } else {
+        user[field.name] = field.value;
+      }
+    });
 
-var country = document.querySelector("#exampleInputCountry");
-
-var email = document.querySelector("#exampleInputEmail");
-
-var password = document.querySelector("#exampleInputPassword");
-
-var photo = document.querySelector("#exampleInputFile");
-
-var admin = document.querySelector("#exampleInputAdmin");
+    console.log(user);
+  });
