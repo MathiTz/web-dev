de45e80c9cb486422cc79453db451b8cd36196ca
Mathitz <matheusalves789@gmail.com>
2019-08-01 16:22:32 -0300

    Adicionar evento de click no botão editar

--- studyJS/curso-javascript-projeto-usuarios/controllers/UserController.js
+++ studyJS/curso-javascript-projeto-usuarios/controllers/UserController.js
@@ -15,6 +15,8 @@ class UserController {
 
       let values = this.getValues();
 
+      if (!values) return false;
+
       this.getPhoto().then(
         content => {
           values.photo = content;
@@ -60,8 +62,17 @@ class UserController {
 
   getValues() {
     let user = {};
+    let isValid = true;
 
     [...this.formEl.elements].forEach(function(field) {
+      if (
+        ["name", "email", "password"].indexOf(field.name) > -1 &&
+        !field.value
+      ) {
+        field.parentElement.classList.add("has-error");
+        isValid = false;
+      }
+
       if (field.name == "gender") {
         if (field.checked) {
           user[field.name] = field.value;
@@ -72,6 +83,8 @@ class UserController {
         user[field.name] = field.value;
       }
     });
+
+    if (!isValid) return false;
 
     return new User(
       user.name,
@@ -88,6 +101,8 @@ class UserController {
   addLine(dataUser) {
     let tr = document.createElement("tr");
 
+    tr.dataset.user = JSON.stringify(dataUser);
+
     tr.innerHTML = `
     <td><img src="${
       dataUser.photo
@@ -103,5 +118,23 @@ class UserController {
   `;
 
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+
+  updateCount() {
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr => {
+      numberUsers++;
+
+      let user = JSON.parse(tr.dataset.user);
+
+      if (user._admin) numberAdmin++;
+    });
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
   }
 }
