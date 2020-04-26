7ee69651f6907f8ee1131b64103f4c7defae2d0c
mathitz <matheusalves789@gmail.com>
2020-04-26 00:01:33 -0300

    Finished backend

---
+++ location-users/backend/controllers/places-controllers.js
@@ -0,0 +1,118 @@
+const uuid = require("uuid/v4");
+const { validationResult } = require("express-validator");
+
+const HttpError = require("../models/http-error");
+const getCoordsForAddress = require('../util/location')
+
+let DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://dicasnovayork.com.br/wp-content/uploads/2016/02/empire_header1-1000x700.jpg",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for provided id", 404);
+  }
+
+  res.json({ place });
+};
+
+const getPlacesByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const places = DUMMY_PLACES.filter((p) => {
+    return p.creator === userId;
+  });
+
+  if (!places || places.length === 0) {
+    return next(new HttpError("Could not find a place for provided id", 404));
+  }
+
+  res.json({ places });
+};
+
+const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
+
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const createdPlace = {
+    id: uuid(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
+const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data", 422);
+  }
+
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
+
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+
+  if (!DUMMY_PLACES.find((p) => p.id === place)) {
+    throw new HttpError("Could not find a place for provided id", 404);
+  }
+
+  DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
+  res.status(200).json({ message: "Deleted place" });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
+exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
---
+++ location-users/backend/util/location.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+const HttpError = require("../models/http-error");
+
+const API_KEY = "8d1899973bf14d0e99ebec9e95becc4a";
+
+async function getCoordsForAddress(address) {
+  // return {
+  //   lat: 40.7484474,
+  //   lng: -73.9871516
+  // };
+  const response = await axios.get(
+    ` https://api.opencagedata.com/geocode/v1/json?q=${address}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422
+    );
+    throw error;
+  }
+
+  const { lat, lng } = data.results[0].geometry;
+  const coordinates = {
+    lat,
+    lng,
+  };
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
